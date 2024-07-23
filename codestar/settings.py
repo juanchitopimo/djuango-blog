9d43394e9c7bfea33eb3ec2336190377cb3f986e
Juan Pinson Moreno <juan@fourrooms.co.uk>
2024-07-23 16:29:09 +0000

    create secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,6 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-so82@#@3a#=42c=#j^9fwb77-76jjvb(@9e5aot53z=_vf%)0i'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,6 +91,8 @@ DATABASES = {
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "postgres://uab6bt6inzc:Jkcz5QsG4SmF@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/repay_vixen_vapor_36283"))
+    
+
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
