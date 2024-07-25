f0ef5807a096ec623bda85c1e533e63ad673b639
Juan Pinson Moreno <juan@fourrooms.co.uk>
2024-07-25 11:55:16 +0000

    update migration test

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,10 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on  = models.DateTimeField(auto_now=True)
+    
+    
+   
+
 
 # Add methods to the Post model
 
