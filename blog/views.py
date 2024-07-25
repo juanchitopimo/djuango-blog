3d34a9b1c5b10fac5b74c86e4ea85ff9e7510349
Juan Pinson Moreno <juan@fourrooms.co.uk>
2024-07-25 18:23:46 +0000

    Create a class-based view update

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .models import Post
 # Create your views here.
 
 class PostList(generic.ListView):
-    model = Post
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
 
    
