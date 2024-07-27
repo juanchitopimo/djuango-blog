dc574c2720adba1d7514dc5bb33b5a6cdf689e14
Juan Pinson Moreno <juan@fourrooms.co.uk>
2024-07-27 17:21:39 +0000

    add AllAuth set up

--- codestar/urls.py
+++ codestar/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("about/", include("about.urls"), name="about-urls"),
+    path("accounts/", include("allauth.urls")),
     path("about/", include("about.urls"), name="about-urls"),
     path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
