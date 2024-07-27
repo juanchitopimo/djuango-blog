4b1b99b35f144751fd6ef4f8a6c2d28b358cdc72
Juan Pinson Moreno <juan@fourrooms.co.uk>
2024-07-27 16:41:04 +0000

    fix import error

--- about/admin.py
+++ about/admin.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
-from .models import Post, Comment, About
+from .models import About
 from django_summernote.admin import SummernoteModelAdmin
 
 
-
-
 @admin.register(About)
-class Admin(SummernoteModelAdmin):
+class AboutAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('content',)
-
-# Register your models here.
-admin.site.register(About)
